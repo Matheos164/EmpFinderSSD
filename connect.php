c5a3ddd5b649353a81cfbcc53121a15b2f7271af
Matheos Amanuel <108306038+Matheos164@users.noreply.github.com>
2024-10-09 17:10:11 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,16 @@
 <?php
-/**
- * Use this file to connect to the database 
- */
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $dbh = new PDO( //change the following 3 lines to connect to a server
-        "mysql:host=employefindergp-server.mysql.database.azure.com;dbname=emp",
-        "wcvhthnpby",
-        "VPQxGTp5jKAcnMj$"
-    );
-} catch (Exception $e) {
-    die("ERROR: Couldn't connect. {$e->getMessage()}");
+    $conn = new PDO("sqlsrv:server = tcp:empfinder.database.windows.net,1433; Database = emp", "matheos", "Password!1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "matheos", "pwd" => "{your_password_here}", "Database" => "emp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:empfinder.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
